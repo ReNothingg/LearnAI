0d9a695ede61d6bfa411c21b105f0f8ad9f73f9a
ReNothing <pashasob2009@gmail.com>
2025-05-22 19:19:38 +0300

    123123

--- Learn2/Урок#3/main.py
+++ Learn2/Урок#3/main.py
@@ -5,7 +5,7 @@ photo = np.zeros((450, 450, 3), dtype='uint8')
 
 #RGB - based
 #BGR - в OpenCV
-photo[:] = 255, 12, 0
+photo[100:150, 200:280] = 255, 12, 0
 
 cv2.imshow('Photo', photo)
 cv2.waitKey(0)
